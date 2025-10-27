641d9ba852282c22a6aacafccfb03534b2f5a742
Sami Agouram <sami.agouram22@gmail.com>
2025-10-27 21:52:29 +0100

    Ajout d'une docstring dans le code

--- model.py
+++ model.py
@@ -1,4 +1,12 @@
 def predict_sentiment(text):
+
+    """
+    Analyse le texte et retourne un sentiment :
+    - 'positive' si le texte contient 'happy' ou 'good'
+    - 'negative' si le texte contient 'sad' ou 'bad'
+    -  sinon 'neutral' 
+    """
+
     if not text:
         return "neutral"
     if "happy" in text.lower() or "good" in text.lower():
